9ff9d3ec0661067422f7dcdcf5291033664bd50b
komekez <kausr2595@gmail.com>
2022-07-13 12:26:17 -0700

    Utils Made

---
+++ src/utils.py
@@ -0,0 +1,17 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torch.autograd import Variable
+
+def lossFunction(recon_x, x, mu, log_var):
+    BCE = F.binary_cross_entropy(recon_x, x.view(-1, 784), reduction='sum')
+    KLD = -0.5 * torch.sum(1 + log_var - mu.pow(2) - log_var.exp())
+
+    return BCE+KLD
+
+
+def oneHotEncoding(labels, class_size):
+    targets = torch.zeros(labels.size(0), class_size)
+    for i, label in enumerate(labels):
+        targets[i, label] = 1
+    return Variable(targets)
