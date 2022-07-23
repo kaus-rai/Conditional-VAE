640705393f12df97ef754627bcd3a7afb2a12363
komekez <kausr2595@gmail.com>
2022-07-23 12:22:50 -0700

    Model Changed to CNN Model

--- src/model.py
+++ src/model.py
@@ -2,40 +2,70 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
+device = torch.device("cuda")
+
 class CVAE(nn.Module):
-    def __init__(self, x_dim, h_dim1, h_dim2, z_dim, c_dim):
+    def __init__(self, latent_size=32, num_class=10):
         super(CVAE, self).__init__()
-        
-        # encoder part
-        self.fc1 = nn.Linear(x_dim + c_dim, h_dim1)
-        self.fc2 = nn.Linear(h_dim1, h_dim2)
-        self.fc31 = nn.Linear(h_dim2, z_dim)
-        self.fc32 = nn.Linear(h_dim2, z_dim)
-        # decoder part
-        self.fc4 = nn.Linear(z_dim + c_dim, h_dim2)
-        self.fc5 = nn.Linear(h_dim2, h_dim1)
-        self.fc6 = nn.Linear(h_dim1, x_dim)
+        self.latent_size = latent_size
+        self.num_class = num_class
+
+        #Encoder
+        self.conv1 = nn.Conv2d(2,16, kernel_size=5, stride=2)
+        self.conv2 = nn.Conv2d(16,32, kernel_size=5, stride=2)
+        self.linear1 = nn.Linear(4*4*32,300)
+        self.mu = nn.Linear(300, self.latent_size)
+        self.logvar = nn.Linear(300, self.latent_size)
+
+        #Decoder
+        self.linear2 = nn.Linear(self.latent_size + self.num_classes, 300)
+        self.linear3 = nn.Linear(300, 4*4*32)
+        self.conv3 = nn.ConvTransponse2d(32, 16, kernel_size=5, stride=2)
+        self.conv4 = nn.ConvTransponse2d(16, 1, kernel_size=5, stride=2)
+        self.conv5 = nn.ConvTransponse2d(1, 1, kernel_size=4)
     
     def encoder(self, x, c):
-        concat_input = torch.cat([x, c], 1)
-        h = F.relu(self.fc1(concat_input))
-        h = F.relu(self.fc2(h))
-        return self.fc31(h), self.fc32(h)
+        y = torch.argmax(y, dim=1).reshape(y.shape[0],1,1,1)
+        y = torch.ones(x.shape).to(device)*y
+        t = torch.cat((x,y), dim=1)
+
+        t = F.relu(self.conv1(t))
+        t = F.relu(self.conv2(t))
+        t = t.reshape(x.shape[0], -1)
+
+        t = F.relu(self.linear1(t))
+        mu = self.mu(t)
+        logvar = self.logvar(t)
+
+        return mu, logvar
+
+    def reparameterize(self, mu, logvar):
+        std = torch.exp(0.5*logvar)
+        eps = torch.randn_like(std).to(device)
+
+        return eps*std +mu
+
+    def unFlatten(self,x):
+        return x.reshape(x.shape[0], 32, 4, 4)
+
+    def decoder(self, z):
+        t = F.relu(self.linear2(z))
+        t = F.relu(self.linear3(t))
+        t = self.unFlatten(t)
+        t = F.relu(self.conv3(t))
+        t = F.relu(self.conv4(t))
+        t = F.relu(self.conv5(t))
+
+        return t
     
-    def sampling(self, mu, log_var):
-        std = torch.exp(0.5*log_var)
-        eps = torch.randn_like(std)
-        return mu+(eps*std) # return z sample
     
-    def decoder(self, z, c):
-        concat_input = torch.cat([z, c], 1)
-        h = F.relu(self.fc4(concat_input))
-        h = F.relu(self.fc5(h))
-        return torch.sigmoid(self.fc6(h))
-    
-    def forward(self, x, c):
-        mu, log_var = self.encoder(x.view(-1, 784), c)
-        z = self.sampling(mu, log_var)
-        return self.decoder(z, c), mu, log_var
+    def forward(self, x, y):
+        mu, log_var = self.encoder(x, y)
+        z = self.reparameterize(mu, logvar)
+
+        #Addition of Class condition
+        z = torch.cat((z, y.float()), dim=1)
+        pred = self.decoder(z)
+        return pred, mu, logvar
 
 
